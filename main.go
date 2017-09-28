121b1aa21f1ed8e0181371bcf802456823af3e82
Wattana Ruengmucha <iamwattanas@gmail.com>
2017-09-29 00:40:33 +0700

    compact code

--- main.go
+++ main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"html/template"
   "net/http"
   "log"
   "github.com/julienschmidt/httprouter"
-  c "./controllers"
 )
+
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	t, _ := template.ParseFiles("views/base.tmpl", "views/index.tmpl")
+	t.Execute(w, nil)
+}
 
 func main()  {
   router := httprouter.New()
-  router.GET("/", c.Index)
+  router.GET("/", Index)
   router.NotFound = http.StripPrefix("/public/", http.FileServer(http.Dir("./static/")))
   log.Fatal(http.ListenAndServe(":8000", router))
 }
